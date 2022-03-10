496ea207bfdcec29d919ef0bee5959d7fb5e6604
alunoESGTS <eduardo_ribeiro2003@hotmail.com>
2022-03-10 17:57:10 +0000

    Parametrizar tamanho da caixa

--- src/caixa/Application.java
+++ src/caixa/Application.java
@@ -3,33 +3,34 @@ package caixa;
 public class Application {
 
 	public static void main(String[] args) {
-		
+		int linhas = 5;
+		int colunas = 5;
 		// TOPO
-		horizontal();
+		horizontal(colunas);
 		
 		// LATERAL
-		vertical();
+		vertical(linhas, colunas);
 		
 		//FIM
-		horizontal();
-		
+		horizontal(colunas);	
 	}
 	
-	private static void horizontal() {
-		for(int i=0; i<9 ; i++) {
+	private static void horizontal(int colunas) {
+		for(int i = 0; i < colunas ; i++) {
 			System.out.print("0");
 		}
 		System.out.println(""); 
 	}
 	
-	private static void vertical() {
-		for(int i=0; i<9 ; i++) {
+	private static void vertical(int linhas, int colunas) {
+		for(int i = 0 ; i < linhas ; i++) {
 			System.out.print("0");
-			System.out.print("       ");
+			for(int x = 0; x < colunas -2; x++ ) {
+			System.out.print(" ");
+			}
 			System.out.print("0");
 			System.out.println("");
 			
 		}
 	}
-
 }
