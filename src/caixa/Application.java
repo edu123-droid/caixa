6758b11aefdbd6290d3047d8ca1732824edc31c7
alunoESGTS <eduardo_ribeiro2003@hotmail.com>
2022-03-10 17:20:30 +0000

    Commit Inicial Caixa

---
+++ src/caixa/Application.java
@@ -0,0 +1,29 @@
+package caixa;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		// TOPO
+		for(int i=0; i<9 ; i++) {
+			System.out.print("0");
+		}
+		System.out.println(""); 
+		
+		// LATERAL
+		for(int i=0; i<9 ; i++) {
+			System.out.print("0");
+			System.out.print("       ");
+			System.out.print("0");
+			System.out.println("");
+			
+		}
+		
+		//FIM
+		for(int i=0; i<9 ; i++) {
+			System.out.print("0");
+		}  
+		
+	}
+
+}
